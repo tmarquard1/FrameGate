8090125e476ff06a36a6d064d50de5d566c38a55
tmarquard1 <tmarquard@10xpeople.com>
2024-10-20 12:20:04 -0600

    use non-relative

--- camera-experiment/camera.py
+++ camera-experiment/camera.py
@@ -2,6 +2,7 @@ from picamzero import Camera
 from time import sleep
 
 cam = Camera()
-cam.start_preview()
+# cam.start_preview()
+cam.take_photo("/home/tmarquard/Documents/FrameGate/camera-experiment/new_image.jpg")
 # Keep the preview window open for 5 seconds
 sleep(5)
