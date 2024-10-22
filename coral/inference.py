7e189c60a3a8133d59cb4519db9fcc70959f5ddd
tmarquard1 <tmarquard@10xpeople.com>
2024-10-21 21:11:13 -0600

    Add .gitignore and update inference.py and docker-compose.yml
    - Added .gitignore file to ignore certain directories and files.
    - Updated inference.py to normalize image array values.
    - Updated docker-compose.yml to include volume mapping and enable initialization.
    
    Closes #123

--- coral/inference.py
+++ coral/inference.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from pycoral.adapters import common
 from pycoral.adapters import classify
 from pycoral.utils.edgetpu import make_interpreter
-from tensorflow.keras.preprocessing import image
+#from tensorflow.keras.preprocessing import image
 import numpy as np
 import io
 from PIL import Image
@@ -19,7 +19,8 @@ async def predict(file: UploadFile = File(...)):
     img = Image.open(io.BytesIO(contents)).resize((224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
+    img_array = (img_array / 127.5) - 1  # Normalize to [-1, 1]
+    # img_array = preprocess_input(img_array)
 
     common.set_input(interpreter, img_array)
     interpreter.invoke()
