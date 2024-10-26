fda3ea3e64661d395899b2f499acadff65057de2
tmarquard1 <tmarquard@10xpeople.com>
2024-10-25 21:19:18 -0600

    camera tests

--- camera-experiment/camera.py
+++ camera-experiment/camera.py
@@ -1,11 +1,22 @@
 from picamzero import Camera
 from time import sleep
+import requests
+import io
+from PIL import Image
 
 cam = Camera()
-# cam.start_preview()
-#cam.flip_camera(hflip=True, vflip=True)
 
-cam.take_photo("/home/tmarquard/Documents/FrameGate/camera-experiment/new_image.jpg")
+# Capture the photo into an in-memory byte stream
+photo_stream = io.BytesIO()
+cam.capture(photo_stream, format='jpeg')
+photo_stream.seek(0)  # Rewind the stream to the beginning
 
-# Keep the preview window open for 5 seconds
-sleep(5)
+# Send the photo to the endpoint
+url = 'http://mathias.local:8000/predict/'
+files = {'file': ('new_image.jpg', photo_stream, 'image/jpeg')}
+headers = {'accept': 'application/json'}
+
+response = requests.post(url, headers=headers, files=files)
+
+# Print the response from the server
+print(response.json())
