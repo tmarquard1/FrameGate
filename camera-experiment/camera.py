68a39d469ffb6b71d1f422e1c7ab6ccaa56ac9fe
tmarquard1 <tmarquard@10xpeople.com>
2024-10-25 21:24:16 -0600

    trying camera stuff out

--- camera-experiment/camera.py
+++ camera-experiment/camera.py
@@ -1,15 +1,27 @@
-from picamzero import Camera
-from time import sleep
+from picamera2 import Picamera2
 import requests
 import io
 from PIL import Image
 
-cam = Camera()
+# Initialize the camera
+picam2 = Picamera2()
+
+# Configure the camera
+config = picam2.create_still_configuration()
+picam2.configure(config)
+
+# Start the camera
+picam2.start()
 
 # Capture the photo into an in-memory byte stream
 photo_stream = io.BytesIO()
-cam.capture(photo_stream, format='jpeg')
+image = picam2.capture_array()
+img = Image.fromarray(image)
+img.save(photo_stream, format='jpeg')
 photo_stream.seek(0)  # Rewind the stream to the beginning
+
+# Stop the camera
+picam2.stop()
 
 # Send the photo to the endpoint
 url = 'http://mathias.local:8000/predict/'
